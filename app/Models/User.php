e6551bd560e4bce056b0dcb892e39c82992e2ea7
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-21 03:30:09 +0300

    feat: add tenant-aware roles to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,13 +12,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     public function tenantMemberships(): HasMany
     {
