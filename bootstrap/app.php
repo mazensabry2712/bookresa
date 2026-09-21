52b0051d857920e41ef401ee83497171010b0346
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-21 04:49:15 +0300

    feat: register platform admin provisioning command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Console\Commands\ExpireSubscriptionsCommand;
+use App\Console\Commands\SetPlatformAdminCommand;
 use App\Http\Middleware\ResolveTenant;
 use App\Http\Middleware\EnsurePlatformAdmin;
 use Illuminate\Console\Scheduling\Schedule;
@@ -14,6 +15,7 @@ use Spatie\Permission\Middleware\PermissionMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withCommands([
         ExpireSubscriptionsCommand::class,
+        SetPlatformAdminCommand::class,
     ])
     ->withSchedule(function (Schedule $schedule): void {
         $schedule->command('subscriptions:expire')
