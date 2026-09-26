d9486c6259a8fa0d0247ff19240ddb6997dab670
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 14:54:52 +0300

    style: import create subscription in module test

--- tests/Feature/PlatformModuleManagementTest.php
+++ tests/Feature/PlatformModuleManagementTest.php
@@ -4,6 +4,7 @@ use App\Domain\Billing\Enums\PlanBillingPeriod;
 use App\Domain\Billing\Enums\SubscriptionStatus;
 use App\Domain\Billing\Models\Plan;
 use App\Domain\Billing\Models\Subscription;
+use App\Domain\Billing\Services\CreateSubscription;
 use App\Domain\Business\Actions\CreateBusiness;
 use App\Domain\Business\Models\BusinessType;
 use App\Domain\Module\Models\Module;
