117619f2bed7aafc6efafb0fb68d4b103a30f30b
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 13:17:24 +0300

    style: satisfy pint empty constructor rule

--- app/Domain/Customer/Actions/UpdateCustomer.php
+++ app/Domain/Customer/Actions/UpdateCustomer.php
@@ -8,14 +8,9 @@ use App\Domain\Tenant\Services\CurrentTenant;
 use LogicException;
 use RuntimeException;
 
-// @php-cs-fixer-ignore single_line_empty_body
 final class UpdateCustomer
 {
-    public function __construct(
-        private readonly CurrentTenant $currentTenant,
-        private readonly CustomerIdentity $identity,
-    ) {
-    }
+    public function __construct(private readonly CurrentTenant $currentTenant, private readonly CustomerIdentity $identity) {}
 
     public function handle(Customer $customer, array $data): Customer
     {
