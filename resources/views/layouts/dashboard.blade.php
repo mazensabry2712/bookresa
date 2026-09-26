34c428abad7ef3ac9082527edda7796f36060653
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 04:05:13 +0300

    security: noindex dashboard pages

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title', config('app.name', 'BookResa'))</title>
+    <meta name="robots" content="noindex,nofollow,noarchive">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
