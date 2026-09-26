df816e94918279a04aa82249a5469bf3f5854366
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 15:13:03 +0300

    feat: add platform settings management

---
+++ app/Domain/Platform/Models/PlatformSetting.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain\Platform\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlatformSetting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+}
