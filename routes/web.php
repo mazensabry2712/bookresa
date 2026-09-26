877e3716527add6156f5fb73dce8f2ac36e488fe
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 15:12:18 +0300

    feat: add platform support routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Platform\PlatformDashboardController;
 use App\Http\Controllers\Platform\PlatformFinanceController;
 use App\Http\Controllers\Platform\PlatformModuleController;
 use App\Http\Controllers\Platform\PlatformUserController;
+use App\Http\Controllers\Platform\SupportTicketController;
 use App\Http\Controllers\PublicBookingController;
 use App\Http\Controllers\Report\ReportController;
 use App\Http\Controllers\Scheduling\SchedulingManagementController;
@@ -211,6 +212,8 @@ Route::middleware(['auth', 'platform'])
         Route::get('/payments', [PlatformFinanceController::class, 'payments'])->name('payments.index');
         Route::get('/usage', [PlatformFinanceController::class, 'usage'])->name('usage.index');
         Route::get('/reports', [ReportController::class, 'platform'])->name('reports.index');
+        Route::get('/support', [SupportTicketController::class, 'index'])->name('support.index');
+        Route::patch('/support/{ticket}', [SupportTicketController::class, 'update'])->name('support.update');
         Route::patch('/businesses/{tenant}/status', [PlatformBusinessController::class, 'toggleStatus'])
             ->name('businesses.toggle-status');
         Route::get('/plans', [PlanAdminController::class, 'index'])->name('plans.index');
