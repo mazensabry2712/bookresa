c2a93ca514920ca0cadeefd1150f8c26b9a14f23
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 15:11:51 +0300

    feat: add platform support management

---
+++ app/Domain/Support/Enums/SupportTicketPriority.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\Support\Enums;
+
+enum SupportTicketPriority: string
+{
+    case Low = 'low';
+    case Normal = 'normal';
+    case High = 'high';
+    case Urgent = 'urgent';
+}
