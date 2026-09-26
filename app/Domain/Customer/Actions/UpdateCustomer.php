f61316103d373b47e3f7b573ece0cb901e3f48a5
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-26 13:14:40 +0300

    style: separate customer update constructor

--- app/Domain/Customer/Actions/UpdateCustomer.php
+++ app/Domain/Customer/Actions/UpdateCustomer.php
@@ -13,7 +13,9 @@ final class UpdateCustomer
     public function __construct(
         private readonly CurrentTenant $currentTenant,
         private readonly CustomerIdentity $identity,
-    ) {}
+    ) {
+    }
+
     public function handle(Customer $customer, array $data): Customer
     {
         $tenantId = $this->currentTenant->idOrFail();
